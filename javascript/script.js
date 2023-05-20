7946e604dfc943a730dfa12b61a4043bdbf2f853
nevesvasco <a043335@umaia.pt>
2023-05-20 16:33:35 +0100

    slideshow

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,19 @@ function updatemenu() {
       document.getElementById('menu').style.borderRadius = '10px';
     }
   }
+  
+  let index = 0;
+  displayImages();
+  function displayImages() {
+    let i;
+    const images = document.getElementsByClassName("image");
+    for (i = 0; i < images.length; i++) {
+      images[i].style.display = "none";
+    }
+    index++;
+    if (index > images.length) {
+      index = 1;
+    }
+    images[index-1].style.display = "block";
+    setTimeout(displayImages, 2000); 
+  }
